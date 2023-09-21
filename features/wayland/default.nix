456e1f1e4fa46e9f1307639db0c8037484831676
Alex S <alex.sustavov@outlook.com>
2023-09-22 00:21:16 +0200

    fixed gsettings

--- features/wayland/default.nix
+++ features/wayland/default.nix
@@ -18,6 +18,7 @@ let
       gnome_schema=org.gnome.desktop.interface
       gsettings set $gnome_schema gtk-theme 'Adwaita'
       gsettings set $gnome_schema color-scheme 'prefer-dark'
+      gsettings set $gnome_schema font-name 'Sans 8'
     '';
   };
 
